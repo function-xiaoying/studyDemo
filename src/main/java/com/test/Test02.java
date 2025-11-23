2aa86f8cdb6268e6d6b8aaa5b7708c6dbe266e2b
function-xiaoying <54209045+function-xiaoying@users.noreply.github.com>
2025-11-23 21:15:21 +0800

    Update Test02.java
    dd

--- src/main/java/com/test/Test02.java
+++ src/main/java/com/test/Test02.java
@@ -11,6 +11,35 @@ public class Test02 {
 
     }
 
+     public static String nullToString2(Object s){
+        if(s == null){
+            return "";
+        }else{
+            return s.toString().trim();
+        }
+
+    }
+
+     public static String nullToString3(Object s){
+        if(s == null){
+            return "";
+        }else{
+            return s.toString().trim();
+        }
+
+    }
+
+     public static String nullToString4(Object s){
+        if(s == null){
+            return "";
+        }else{
+            return s.toString().trim();
+        }
+
+    }
+
+    
+
 
 
 }
